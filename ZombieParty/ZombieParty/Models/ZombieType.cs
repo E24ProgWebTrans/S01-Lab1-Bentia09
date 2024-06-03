e3048162e221c6fae3a978c80d1755b9cabb14ad
Bentia <1548884@cegepmontpetit.ca>
2024-06-03 14:11:24 -0400

    FCT classe ZombieType
    Ajout de la classe ZombieType

---
+++ ZombieParty/ZombieParty/Models/ZombieType.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZombieParty.Models;
+
+
+
+namespace ZombieParty.Models
+{
+    public class ZombieType
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+
+
+
+    }
+}
