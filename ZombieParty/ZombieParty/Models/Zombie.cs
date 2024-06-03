d3a41a3785ce6b203890627803e4d036bac63ab0
Bentia <1548884@cegepmontpetit.ca>
2024-06-03 14:09:36 -0400

    FCT Zombie Index
    Ajout du contrôleur Zombie et de la vue Index associée affichant la liste des zombies. Ajout du menu de gestion avec Bootstrap.

--- ZombieParty/ZombieParty/Models/Zombie.cs
+++ ZombieParty/ZombieParty/Models/Zombie.cs
@@ -1,6 +1,17 @@
-﻿namespace ZombieParty.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using ZombieParty.Models;
+
+
+namespace ZombieParty.Models
 {
     public class Zombie
     {
+          public  string Name { get; set; }
+        public string Type { get; set; }
+        public int Point { get; set; }
+
+
+
+
     }
 }
